a56b59df8c2845f521c8293e1f0ed17132845c4b
封铃解滢 <495950007@qq.com>
2024-06-08 23:04:22 +0800

    Add JSON validator function to remove specified keys from JSON strings

--- ai_commons/file_processing/json_validator.py
+++ ai_commons/file_processing/json_validator.py
@@ -1,1 +1,17 @@
-# JSON validator module
+# Json validator module
+
+import json
+
+def remove_keys_from_json(json_string, keys_to_remove):
+    # 解析JSON字符串为列表
+    data_list = json.loads(json_string)
+    
+    # 遍历列表中的每个字典
+    for data in data_list:
+        for key_list in keys_to_remove:
+            for key in key_list:
+                if key in data:
+                    del data[key]
+    
+    # 将列表转换回JSON字符串
+    return json.dumps(data_list, ensure_ascii=False)
